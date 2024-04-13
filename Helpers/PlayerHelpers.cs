620dd9c8c52643a8082c484098e6c91db3e4b032
Austin Smith <austint.smith48@gmail.com>
2024-04-12 19:06:23 -0500

    Added helper for killing players in vehicles

---
+++ Helpers/PlayerHelpers.cs
@@ -0,0 +1,15 @@
+﻿using BattleBitApi.Api;
+
+namespace BattleBitApi.Helpers;
+
+public class PlayerHelpers
+{
+    public static void KillPlayerInVehicle(BattleBitApiPlayer player, string reason)
+    {
+        if (player.InVehicle == false)
+            return;
+
+        player.Kill();
+        player.SayToChat(reason);
+    }
+}
